8e824704dc0f1556cbed5750650028eb6d5fd612
SilviaPabon24 <silvia.juliana24@hotmail.com>
2021-10-22 13:08:50 -0500

    Fixing flutter no-sound-null-safety

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -21,19 +21,19 @@ class Auth implements BaseAuth {
   Future<String> signIn(String email, String password) async {
     UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(
         email: email, password: password);
-    User user = result.user;
+    User user = result.user!;
     return user.uid;
   }
 
   Future<String> signUp(String email, String password) async {
     UserCredential result = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email, password: password);
-    User user = result.user;
+    User user = result.user!;
     return user.uid;
   }
 
   Future<User> getCurrentUser() async {
-    User user = await _firebaseAuth.currentUser;
+    User user = await _firebaseAuth.currentUser!;
     return user;
   }
 
@@ -42,12 +42,12 @@ class Auth implements BaseAuth {
   }
 
   Future<void> sendEmailVerification() async {
-    User user = await _firebaseAuth.currentUser;
+    User user = await _firebaseAuth.currentUser!;
     user.sendEmailVerification();
   }
 
   Future<bool> isEmailVerified() async {
-    User user = await _firebaseAuth.currentUser;
+    User user = await _firebaseAuth.currentUser!;
     return user.emailVerified;
   }
 }
